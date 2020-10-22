d41ba42dee6c28ba4ec52fffd48606127cf95900
apo23 <apo.roubert@gmail.com>
2020-10-22 17:01:24 +0200

    Update agc.py, PARTIE 2 NON FINIE

--- agc/agc.py
+++ agc/agc.py
@@ -110,8 +110,16 @@ def dereplication_fulllength(amplicon_file, minseqlen, mincount):
         yield_index.pop(curr[0])
         
 
-def test_get_chunks():
-    pass
+def get_chunks(sequence, chunk_size):
+    chunk_num = int(len(sequence) / chunk_size)
+    chunk_list = []
+    if chunk_num < 4:
+        chunk_num = int(len(sequence) / 4)
+    count = 0
+    for i in range(chunk_num):
+        chunk_list.append(sequence[count:count+chunk_size])
+        count += chunk_size
+    return chunk_list
 
 
 def test_unique():
@@ -122,28 +130,79 @@ def test_common():
     pass
 
 
-def test_cut_kmer():
-    pass
+def cut_kmer(sequence, kmer_size):
+    for i in range(len(sequence)-(kmer_size-1)):
+        yield sequence[i:i+kmer_size]
 
 
-def test_get_unique_kmer():
-    pass
+def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
+    kmers = cut_kmer(sequence, kmer_size)
+    for kmer in kmers:
+        if kmer not in kmer_dict:
+            kmer_dict[kmer] = [id_seq]
+        else:
+            kmer_dict[kmer].append(id_seq)
+    return kmer_dict
 
 
-def test_search_mates():
-    pass
+def search_mates(kmer_dict, sequence, kmer_size):
+    return [i[0] for i in Counter([ids for kmer in cut_kmer(sequence, kmer_size) if kmer in kmer_dict for ids in kmer_dict[kmer]]).most_common(8)]
 
 
-def test_get_identity():
-    pass
+def get_identity(alignment_list):
+    diffs = [x for x in alignment_list[0] if x != y for y in alignment_list[1]]
+    ids = len(diffs)
+    return ids/len(alignment_list[0])
 
 
-def test_detect_chimera():
-    pass
+def detect_chimera(perc_identity_matrix):
+    stdevs = []
+    diff_count = 0
+    favored = -1
+    prev_fav = -1
+    switch_flag = 0
+    for i in range(len(perc_identity_matrix)):
+        curr_line = perc_identity_matrix[i]
+        if prev_fav != favored:
+            switch_flag += 1
+        prev_fav = favored
+        favored = [i for i, x in enumerate(curr_line) if x == max(curr_line)][0]
+        curr_stdev = statistics.stdev(curr_line)
+        stdevs.append(curr_stdev)
+    if prev_fav != favored:
+        switch_flag += 1
+    if switch_flag >= 2 and statistics.mean(stdevs) > 5:
+        return True
+    return False
 
 
-def test_chimera_removal():
-    pass
+def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
+    derep = dereplication_fulllength(amplicon_file, minseqlen, mincount)
+    seqs = []
+    kmer_dict = {}
+    id_seq = 0
+    for seq, occ in derep:
+        is_chimera = False
+        tmp_id_mat = []
+        tmp_chunks = get_chunks(seq, chunk_size)
+        mates = search_mates(kmer_dict, sequence, kmer_size)
+        if len(mates) >= 2:
+            parent1 = seqs[mates[0]]
+            parent2 = seqs[mates[1]]
+
+            p1chunks = get_chunk(parent1[0], chunk_size)
+            p2chunks = get_chunk(parent2[0], chunk_size)
+            for c in range(len(tmp_chunks)): 
+                line = []
+                line.append(get_identity(global_align(seq, parent1[0])))
+                line.append(get_identity(global_align(seq, parent2[0])))
+                tmp_id_mat.append(line)
+            is_chimera = detect_chimera(tmp_id_mat)
+        kmer_dict = get_unique_kmer(kmer_dict, seq, id_seq, kmer_size
+        if is_chimera == False:
+            seqs.append([seq,occ])
+            yield([seq, occ])
+        
 
 
 def test_abundance_greedy_clustering():
@@ -162,8 +221,14 @@ def main():
     """
     # Get arguments
     args = get_arguments()
-    dereplication = dereplication_fulllength(args.amplicon_file, args.minseqlen,
-                                    args.mincount)
+    test = dereplication_fulllength(args.amplicon_file, args.minseqlen, args.mincount)
+    seqs = []
+    count=0
+    for t in test:
+        print(count)
+        count+=1
+        seqs.append(t)
+    print(len(seqs))
     
     
 
